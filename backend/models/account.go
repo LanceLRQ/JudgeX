f9e0fa0d242df5a1a34fc1a15f7961bec0848720
lanrongqi <lancelrq@gmail.com>
2020-06-12 17:45:35 +0800

    add: account models

---
+++ backend/models/account.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+// 用户账户表表
+type Account struct {
+	gorm.Model
+	UserName 			string	`gorm:"unique;not null"`	// 登录名称
+	PassWord 			string								// 登录密码
+	Email 				string								// 登录邮箱
+	EmailValidated 		bool								// 邮箱是否验证
+	Locked 				bool								// 账号锁定
+
+	IsAdmin 			bool								// 系统管理员身份
+	PermCreateProblem 	bool								// 创建题目
+	PermManageProblems 	bool								// 管理非自己创建的题目
+	PermProblemDatas 	bool 								// 查看非自己创建题目的数据
+}
+
+// 登录日志表
+type LoginLog struct {
+	ID        		uint `gorm:"primary_key"`				// 主键
+	CreatedAt 		time.Time								// 创建时间
+	IpAddress 		string									// IP地址
+	LoginAccount 	Account									// 登录用户
+	UserAgent 		string 									// UA信息
+}
+
+// WeJudge SSO服务绑定信息表
+// WeJudge SSO service binding records.
+type WeJudgeSSOBinding struct {
+	ID        			uint `gorm:"primary_key"`			// 主键
+	LoginAccount 		Account								// 登录用户
+	WeJudgeAccountId	string								// WeJudge 账号Id
+}
